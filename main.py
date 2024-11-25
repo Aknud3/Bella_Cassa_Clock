755d5452d8ebe539b030c73ea9b4f568ed250ff8
Eduard Wojnar <168775964+Aknud3@users.noreply.github.com>
2024-11-25 10:34:35 +0100

    Napsání komentů
    Všude jsem napsal komenty

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# Vyrobil Eduard Wojnar
+
+#import všech knihoven
 from time import sleep_ms, time, sleep
 from machine import RTC, Pin, WDT
 import machine
@@ -7,42 +10,47 @@ import socket
 import ntptime
 import gc
 
+# tisknutí volné paměti v čipu
 print(gc.mem_free())
 
-# tady potřebuju oranžové barvy aby se to hodilo do učebny
+# Nastavení všech barev ledek
+#GRB
+COLOR_OF_ALPHAS = (184*5, 255*5, 0) # barvy písmen
+COLOR_OF_ALPHAS_TIMER = (0, 500, 0) # barvy písmen v timeru
 
-COLOR_OF_ALPHAS = (184*5, 255*5, 0)  # green, red, blue format
-COLOR_OF_ALPHAS_TIMER = (0, 500, 0)
+# RGB
+COLOR_OF_SECONDS = (2, 2, 2) # barvy sekund
+COLOR_OF_CORNERS = (0, 0, 64) # barvy rohů
+COLOR_OF_BORDER_TIMER = (128, 0, 0) # barva sekund v timeru
+COLOR_OF_AMBIENT = (0, 0, 0) # barvy ambientního osvětlení
 
-COLOR_OF_SECONDS = (2, 2, 2) # RGB
-COLOR_OF_CORNERS = (0, 0, 64)
-COLOR_OF_BORDER_TIMER = (128, 0, 0)
-COLOR_OF_AMBIENT = (0, 0, 0)
+COLOR_OF_SUPPORT_ALPHAS = (51, 30, 0) # Barvy pomocných písmen (poslední řada písmenek
+COLOR_OF_SUPPORT_ALPHAS_TIMER = (100, 0, 0) # barvy pomocných písmen v timeru
 
-COLOR_OF_SUPPORT_ALPHAS = (51, 30, 0)
-COLOR_OF_SUPPORT_ALPHAS_TIMER = (100, 0, 0)
+FIRST_CORNER_INDEX = 1 # index prvního rohu
+SECOND_CORNER_INDEX = 33 # index druhého rohu
+THIRD_CORNER_INDEX = 65 # index třetího rohu
+FOURTH_CORNER_INDEX = 97 # index čtvrtého rohu
 
-FIRST_CORNER_INDEX = 1
-SECOND_CORNER_INDEX = 33
-THIRD_CORNER_INDEX = 65
-FOURTH_CORNER_INDEX = 97
+BUTTON_PRESS = 2 # tlačítko, je vypnuto můžete ignorovat
 
-BUTTON_PRESS = 2
+WIFI_LOGINS = [["GMH", "covidgmh"], ["ESP", "espgmhco2"]] # hesla na wifi
+wdt = WDT(timeout=100000000) # vypnutý timeout, protože bez toho to padá
+wdt.feed() # bez tohohle to padá kvůli timeoutu
+RTC_MODULE = RTC() # deklarace RTC modulu
 
-WIFI_LOGINS = [["GMH", "covidgmh"], ["ESP", "espgmhco2"]]
-wdt = WDT(timeout=100000000)
-wdt.feed()
-RTC_MODULE = RTC()
-
-NEOLED = Pin(15, Pin.OUT)
-NEOLED_SUPPORT = Pin(16, Pin.OUT)
-NEOLED_SECONDS = Pin(13, Pin.OUT)
-BUTTON = Pin(27, Pin.IN)
+# Určení na jakém pinu jsou věci
+NEOLED = Pin(15, Pin.OUT)  # na pinu 15 jsou písmenka
+NEOLED_SUPPORT = Pin(16, Pin.OUT) # na pinu 16 jsou pomocná písmenka
+NEOLED_SECONDS = Pin(13, Pin.OUT) # na pinu 13 jsou sekundy
+BUTTON = Pin(27, Pin.IN) # na pinu 27 je tlačítko
 
 
-NEOPIXEL_MAIN = neopixel.NeoPixel(NEOLED, 110)
-NEOPIXEL_SECONDS = neopixel.NeoPixel(NEOLED_SECONDS, 140)
-NEOPIXEL_MAIN_SUPPORT = neopixel.NeoPixel(NEOLED_SUPPORT, 11)
+NEOPIXEL_MAIN = neopixel.NeoPixel(NEOLED, 110) # písmenka
+NEOPIXEL_SECONDS = neopixel.NeoPixel(NEOLED_SECONDS, 140) # sekundy
+NEOPIXEL_MAIN_SUPPORT = neopixel.NeoPixel(NEOLED_SUPPORT, 11) # pomocná písmenka
+
+# Toto je slovník demon, který určuje jaká ledka se rozsvítí v sekundách 
 
 DEMON = {
     0: FIRST_CORNER_INDEX,
@@ -106,6 +114,8 @@ DEMON = {
     58: 123,
     59: 125,
 }
+
+# Tady jsou zbylé slovníky, které určují jaké ledky písmenkách se rozsvěcujou. 3, 37 - T, 38 - Ř, 39 - I etc.
 DICTIONARY = [
     {
         0: [26, 27, 28, 29, 30, 31, 32],
@@ -134,6 +144,7 @@ DICTIONARY = [
         23: [47, 48, 49, 50, 51, 52, 53],
         24: [26, 27, 28, 29, 30, 31, 32],
     },
+    # Tady jsou na sekundy 10, 61 - D, 62 - E... 
     {
         0: [],
         5: [84, 85, 86, 87, 107, 108, 109],
@@ -150,7 +161,7 @@ DICTIONARY = [
     },
 ]
 
-
+# toto se používá pro timer
 FIVE = (
     (14, 15, 16),
     (61, 62, 63, 64, 65),
@@ -173,24 +184,24 @@ FIFTEEN = (
     (47, 48, 49, 50, 51, 52, 53, 54),
 )
 
-
+# Nastavení sítě (Tento kód je Pana Jelínka, který programoval k čidlu vzduchu
 network.phy_mode(network.MODE_11G)
 ap = network.WLAN(network.AP_IF)
 ap.active(False)
 station = network.WLAN(network.STA_IF)
 station.active(True)
 
-
+# Funkce Pana Jelínka, co zjišťuje jestli jsme připojení k internetu
 def isconnected():
     "function to check if we are connected to wifi"
     return station.isconnected()
 
-
+# Funkce Pana Jelínka, co nám získá mac adresu
 def getmac():
     "function to get mac address"
     return station.config("mac")
 
-
+# FUnkce Pana Jelínka, co řídí připojení k internetu
 def wifi_connect(ssid, pswd):
     "function to connect to wifi"
     sta_if = network.WLAN(network.STA_IF)
@@ -212,14 +223,14 @@ def wifi_connect(ssid, pswd):
     print("Connection attempt failed")
     return False
 
-
+# Funkce Pana Jelínka, pro odpojení od Wifi
 def disconnect():
     "function to disconnect from wifi"
     station.active(False)
     if station.isconnected() is False:
         print("Disconnected")
 
-
+# Funkce Pana Jelínka, pro připojení k wifi
 def connect():
     "function to connect to wifi"
     for wifi_essid, wifi_pwd in WIFI_LOGINS:
@@ -229,71 +240,78 @@ def connect():
         if isconnected():
             break
 
-
+# Funkce, co řídí letní a zimní čas
 def hour_offset(current_time):
     "function to get hour offset from winter to summer time"
-    summer_time = False
-    if current_time[1] in [4, 5, 6, 7, 8, 9]:
+
+    # funguje na principu dopočítávání dnů v měsící 
+    summer_time = False # defaultně je nastaven na zimní čas
+    
+    if current_time[1] in [4, 5, 6, 7, 8, 9]: # Jestli jsou tyto měsíce tak je automaticky letní čas
         summer_time = True
-    elif current_time[1] in [11, 12, 1, 2]:
+    elif current_time[1] in [11, 12, 1, 2]: # Jestli jsou tyto měsíce tak je automaticky, tak se nic nestane a je zimní čas
         pass
-    elif current_time[1] == 3 and current_time[2] >= 25:
-        if 7 - current_time[3] + current_time[2] >= 31:
-            summer_time = True
+        
+    elif current_time[1] == 3 and current_time[2] >= 25: # Jestli je tento měsíc a jsou tyto dny
+        if 7 - current_time[3] + current_time[2] >= 31: # funguje tam nějaká logika, co už si nepamatuju jak jsem vymýšlel, ale je to nějak spojené s nedělemi
+            summer_time = True 
         else:
             pass
-    elif current_time[1] == 10 and current_time[2] <= 25:
+            
+    elif current_time[1] == 10 and current_time[2] <= 25: # Stejná logika jako o řádky nahoře, akorát pro jiný měsíc
         if 7 - current_time[3] + current_time[2] >= 31:
             pass
         else:
             summer_time = True
 
-    if summer_time:
+    if summer_time: # Protože bereme Anglický čas, ak letní je 2+ a zimní 1+
         current_time[4] += 2
     else:
         current_time[4] += 1
 
-
+# Funkce timeru, kterou jsem tady nechal, zapíná se tlačítkem
 def count_down(list_of_word_for_countdown):
     "function to count down"
-    # tady je hodně velkej demon
+    # tady je hodně velkej demon (koment pro mě, co mám opravi)
+
+    # Tohle funguje na principu, že tam hodím list a podle toho se mi rozsvítí nějaké písmenka
     light_alphas_timer(list_of_word_for_countdown[0])
-    for k in range(5):
-        NEOPIXEL_SECONDS.fill(COLOR_OF_BORDER_TIMER)
+    for k in range(5): # Vše funguje v 5 minutovém formátu
+        NEOPIXEL_SECONDS.fill(COLOR_OF_BORDER_TIMER) # vše se zbarvní do barvy a potom každou sekundu se jedna ledka vypne, jakoby odpočítává
         NEOPIXEL_SECONDS.write()
         for j in range(60):
             NEOPIXEL_SECONDS[DEMON[59 - j]] = (0, 0, 0)
-            sleep(1)
-            NEOPIXEL_SECONDS.write()
+            sleep(1) 
+            NEOPIXEL_SECONDS.write() # odpočítává
         if k < 4:
-            light_alphas_timer(list_of_word_for_countdown[k + 1])
+            light_alphas_timer(list_of_word_for_countdown[k + 1]) # dostaneme se na další písmenka 
 
-        if k == 4:
+        if k == 4: # Jestli jsme u posledního indexu, tak se podíváme, jaký timer jsme si vybrali, jestli 5 tak se funkce ukončí, jestli 10, tak jede timer pro 5 minut, jestli 15 tak jede timer pro 10 minut, proto to je v 5 minutovém formátu, abych tuto funkci mohl použít víckrát
             if list_of_word_for_countdown == FIVE:
                 NEOPIXEL_MAIN.fill((0, 0, 0))
             elif list_of_word_for_countdown == TEN:
                 count_down(FIVE)
             elif list_of_word_for_countdown == FIFTEEN:
                 count_down(TEN)
-        NEOPIXEL_MAIN.write()
+        NEOPIXEL_MAIN.write() #vše se zapíše do ledek
 
-
+# pouze pomocná funkce, co mi pomáhá rozsvítit ledky, používá se hodně v kodu
 def light_alphas_timer(area):
     "function to light alphas"
-    NEOPIXEL_MAIN.fill((0, 0, 0))
-    for j in area:
+    NEOPIXEL_MAIN.fill((0, 0, 0)) # vše nastaví na bezbarvy
+    for j in area: # podle toho, co do funkce vložíme, to se rozsvítí na tu barvu, co chceme
         NEOPIXEL_MAIN[j] = COLOR_OF_ALPHAS_TIMER
-    NEOPIXEL_MAIN.write()
+    NEOPIXEL_MAIN.write() # zase zapíšeme
 
-
+# Funkce, co nám řídí jak moc velký countdown chceme
 def count_down_mode():
     "function to count down mode"
-    mode = 0
-    for neo in (NEOPIXEL_MAIN, NEOPIXEL_SECONDS, NEOPIXEL_MAIN_SUPPORT):
+    mode = 0 # defaultní mod je 0
+    for neo in (NEOPIXEL_MAIN, NEOPIXEL_SECONDS, NEOPIXEL_MAIN_SUPPORT): # Vše zhasne
         neo.fill((0, 0, 0))
         neo.write()
-    light_alphas_timer((14, 15, 16))
-    for _ in range(10):
+    light_alphas_timer((14, 15, 16)) # rozsvítíme pět protože to je defaultní 
+    for _ in range(10): # 5 sekund čekáme a jestli během toho někdo znova zmáčkne tlačítko, tak se mod dá + 1 a rozsvítí se deset nebo patnáct
         sleep(0.5)
         if BUTTON.value() == BUTTON_PRESS:
             sleep(0.5)
@@ -304,73 +322,87 @@ def count_down_mode():
                 light_alphas_timer((81, 82, 83, 84, 85, 86, 87))
         sleep(0.5)
 
-    if mode == 0:
+    if mode == 0: # Jestli je mode stále 0, tak je pět minut timer
         count_down(FIVE)
-    elif mode == 1:
+    elif mode == 1: # Jestli je mode 1 tak je timer na 10 minut
         count_down(TEN)
-    else:
+    else: # Jestli to je cokoliv jiného tak je timer na 15 minut
         count_down(FIFTEEN)
+        
+    # Tato funkce tady stále je, ale protože je tlačítko vypnuté, tak se nepoužívá
 
-
+# Toto je hlavní funkce hodin
 def clock_mode():
-    "function to clock mode"
-    current_time = list(RTC_MODULE.datetime())
-    hour_offset(current_time)
-    hours = current_time[4]
-    minutes =  current_time[5]
-    clock_text = [0] * 110
-    clock_dots = [False] * 4
+    "function to clock mode" 
+    current_time = list(RTC_MODULE.datetime()) #Vememe si čas z knihovny
+    hour_offset(current_time) # Hodíme ho do funkce hour_offset aby nám to dalo víc hodin
+    hours = current_time[4] # Vememe si z času hodiny 
+    minutes =  current_time[5] # Vememe si z času minuty
+    clock_text = [0] * 110 # Uděláme si pole
+    clock_dots = [False] * 4 # Uděláme si pole pro minuty 
+
+    # Tady tenhle loop z minut normálních dělá rohy (nevím už jak to funuje, je to nějaká demonovina)
     for j in range(1, 5):
         if minutes in [5 * x + j for x in range(0, 12)]:
             for k in range(j):
                 clock_dots[k] = True
-    minutes = (minutes // 5) * 5
+                
+    minutes = (minutes // 5) * 5 # zaokrouhlíme si minuty bez pětek
+
+    # Toto je demon 
+    # Tohle mi funguje s těma minutama a slovníkama to převede 
     filtered_clock_text = set(
         DICTIONARY[0].get(hours, []) + DICTIONARY[1].get(minutes, [])
     )
     result_list = [index in filtered_clock_text for index in range(len(clock_text))]
-    if hours in [0, 1, 5, 6, 7, 8, 9, 10, 11, 12, 13, 17, 18, 19, 20, 21, 22, 23, 24]:
+    if hours in [0, 1, 5, 6, 7, 8, 9, 10, 11, 12, 13, 17, 18, 19, 20, 21, 22, 23, 24]: # Jestli jsou tyhle hodiny, tak se rozsvítí JE
         for j in range(2):
             result_list[j] = True
     else:
-        for j in range(2, 6):
+        for j in range(2, 6): # Jinak se rozsvítí JSOU
             result_list[j] = True
+            
+    # Tohle mi rozdělí ten velkej list na menší listy        
     lists = [result_list[i : i + 11] for i in range(0, len(result_list), 11)]
-    for j, sublist in enumerate(lists):
+    for j, sublist in enumerate(lists): # Protože podle toho jak tam jsou ledky nalepené, tak se to tam lepí do hada a proto vždy sudé se musí obrátit zrcadlově
         if j % 2 == 1:
             lists[j] = sublist[::-1]
 
+    # Tohle mi vše resetuje
     NEOPIXEL_MAIN.fill((0, 0, 0))
     NEOPIXEL_SECONDS.fill((0, 0, 0))
     NEOPIXEL_MAIN_SUPPORT.fill((0, 0, 0))
 
-    # set ambient lighting
+    # Nastaví ambientního osvětlení
     for j in range(140):
         if j % 2 != 1:
             NEOPIXEL_SECONDS[j] = COLOR_OF_AMBIENT
 
-    row = 0
-    for sublist in lists:
-        for b, value in enumerate(sublist):
-            if value is True:
-                index = b + 11 * row
-                if index < 99:
+    # Tohle je další demon
+    row = 0 # řada je první
+    for sublist in lists: # pro sublist v listech
+        for b, value in enumerate(sublist): # podívám se na idex i co v tom je 
+            if value is True: # jestli value je pravdivá
+                index = b + 11 * row # Nastaví index podle řady
+                if index < 99: # Jestli to je menší jak 99, tak je to normální led pásek
                     NEOPIXEL_MAIN[index] = COLOR_OF_ALPHAS
-                elif 110 >= index and index >= 99:
-                    NEOPIXEL_MAIN_SUPPORT[(index - 99)] = COLOR_OF_SUPPORT_ALPHAS
-        row += 1
+                elif 110 >= index and index >= 99: # jestli to je mezi tímto tak to je pro podporný pásek
+                    NEOPIXEL_MAIN_SUPPORT[(index - 99)] = COLOR_OF_SUPPORT_ALPHAS # ta matika tam je aby se odečetl ten index a normálně to fungovalo
+        row += 1 # přičte se čada v tom listu
 
-    # tady bude ultra demonek
-    corners = 0
+    # tady bude ultra demonek (tady byl ultra demonek, ale tohle už je opravený)
+    
+    #tady s tím jsem se moc neprděl, je to extrémně jednoduchej kod 
+    corners = 0 # podle toho jaký jsou minuty v rozích tak, to se rozsvítí
     for content in clock_dots:
         if content is True:
             corners += 1
         else:
             pass
 
-    if corners == 1:
+    if corners == 1: # jestli jedna, tak jenom jedna ledka
         NEOPIXEL_SECONDS[FIRST_CORNER_INDEX] = COLOR_OF_CORNERS
-    elif corners == 2:
+    elif corners == 2: #Jestli dvě, tak dvě se rozsvítí etc.
         for i in (FIRST_CORNER_INDEX, SECOND_CORNER_INDEX):
             NEOPIXEL_SECONDS[i] = COLOR_OF_CORNERS
     elif corners == 3:
@@ -380,46 +412,50 @@ def clock_mode():
         for i in (FIRST_CORNER_INDEX, SECOND_CORNER_INDEX, THIRD_CORNER_INDEX, FOURTH_CORNER_INDEX):
             NEOPIXEL_SECONDS[i] = COLOR_OF_CORNERS
             
-    NEOPIXEL_SECONDS.write()
+    NEOPIXEL_SECONDS.write() # vše zapíšu
 
-    NEOPIXEL_MAIN.write()
+    NEOPIXEL_MAIN.write() # 
     NEOPIXEL_MAIN_SUPPORT.write()
-    return current_time, corners
+    return current_time, corners # tahle funkce mi do main loopu vrací current_time a rohy minut
 
 
-wdt.feed()
-print("System starting...")
-while not isconnected():
+wdt.feed() # vyčistím pamět, protože jinak to padá
+print("System starting...") # print na start systému
+while not isconnected(): # funkce na connect k internetu
     connect()
 print("Connected")
-ntptime.settime()
+ntptime.settime() # vememe si čas
 
-print("Displaying time...")
+print("Displaying time...") # print, že hodiny fungujou
 
-while True:
-    wdt.feed()
-    current_time_list, cornerss = clock_mode()
-    if current_time_list[6] == 1:
+while True: # Hlavní loop 
+    wdt.feed() # zase pamět
+    current_time_list, cornerss = clock_mode() # spustíme logiku
+    if current_time_list[6] == 1: # Protože čas někdy z internetu buguje a nezačíná na 0 ale 1, tak tohle ho upraví aby sekudny jeli dokonce
         current_time_list[6] -= 1
-    for i in range(60 - current_time_list[6]):
+    for i in range(60 - current_time_list[6]): # tohle nám synchronizuje sekundy
         
-        NEOPIXEL_SECONDS[DEMON[i]] = COLOR_OF_SECONDS
+        NEOPIXEL_SECONDS[DEMON[i]] = COLOR_OF_SECONDS # podívá se do démona
         
-        if cornerss >= 1  and i != 0:
+        if cornerss >= 1  and i != 0: # nastavuje sekundy aby to tam probliklo
             NEOPIXEL_SECONDS[FIRST_CORNER_INDEX] = COLOR_OF_CORNERS
                 
-        if cornerss >= 2 and i != 15:
+        if cornerss >= 2 and i != 15: # Nastavuje sekundy aby to probliklo
             NEOPIXEL_SECONDS[SECOND_CORNER_INDEX] = COLOR_OF_CORNERS
         if cornerss >= 3 and i != 30:
             NEOPIXEL_SECONDS[THIRD_CORNER_INDEX] = COLOR_OF_CORNERS
         if cornerss == 4 and i != 45:
             NEOPIXEL_SECONDS[FOURTH_CORNER_INDEX] = COLOR_OF_CORNERS
-        
-        print(i)
+            
+        # Tohle je aby se rozsvítila poslední ledka kdyby to bugovalo
         if i == 59:
             NEOPIXEL_SECONDS[125] = COLOR_OF_SECONDS
         NEOPIXEL_SECONDS.write()
-        sleep(1)
+        sleep(1) # sleep 1 sekunda aby to čekalo 
+
+
+
+# Tohle jsem vypnul proto to je odkomentované je to pro tlačítko
 
   #if BUTTON.value() == BUTTON_PRESS:
            # print("This is countdown")
@@ -439,4 +475,4 @@ while True:
                   #  break
            # break
 
-
+# Jestli to někdo dočetl až sem tak klobouk dolů
